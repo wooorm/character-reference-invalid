83f2a31a59f6380d8d1e218eb54389528dd9f414
Titus Wormer <tituswormer@gmail.com>
2018-04-18 16:57:30 +0200

    Refactor code-style

--- build.js
+++ build.js
@@ -1,34 +1,38 @@
-'use strict';
+'use strict'
 
-var fs = require('fs');
-var https = require('https');
-var bail = require('bail');
-var concat = require('concat-stream');
-var unified = require('unified');
-var parse = require('rehype-parse');
-var selectAll = require('hast-util-select').selectAll;
-var toString = require('hast-util-to-string');
+var fs = require('fs')
+var https = require('https')
+var bail = require('bail')
+var concat = require('concat-stream')
+var unified = require('unified')
+var parse = require('rehype-parse')
+var selectAll = require('hast-util-select').selectAll
+var toString = require('hast-util-to-string')
 
 var data = {
   0: '�'
-};
+}
 
-https.get('https://html.spec.whatwg.org/multipage/parsing.html', function (res) {
-  res.pipe(concat(onconcat)).on('error', bail);
+https.get('https://html.spec.whatwg.org/multipage/parsing.html', onconnection)
 
-  function onconcat(buf) {
-    var tree = unified().use(parse).parse(buf);
+function onconnection(res) {
+  res.pipe(concat(onconcat)).on('error', bail)
+}
 
-    selectAll('#table-charref-overrides tbody tr', tree).forEach(each);
+function onconcat(buf) {
+  var tree = unified()
+    .use(parse)
+    .parse(buf)
 
-    function each(row) {
-      var cells = selectAll('td', row);
-      var numeric = parseInt(toString(cells[0]).slice(2), 16);
-      var char = String.fromCharCode(parseInt(toString(cells[1]).slice(2), 16));
+  selectAll('#table-charref-overrides tbody tr', tree).forEach(each)
 
-      data[numeric] = char;
-    }
+  function each(row) {
+    var cells = selectAll('td', row)
+    var numeric = parseInt(toString(cells[0]).slice(2), 16)
+    var char = String.fromCharCode(parseInt(toString(cells[1]).slice(2), 16))
 
-    fs.writeFile('index.json', JSON.stringify(data, 0, 2) + '\n', bail);
+    data[numeric] = char
   }
-});
+
+  fs.writeFile('index.json', JSON.stringify(data, null, 2) + '\n', bail)
+}
--- test.js
+++ test.js
@@ -1,12 +1,12 @@
-'use strict';
+'use strict'
 
-var test = require('tape');
-var characterReferenceInvalid = require('.');
+var test = require('tape')
+var characterReferenceInvalid = require('.')
 
-test('characterEntities', function (t) {
-  t.equal(characterReferenceInvalid[0x80], '€');
-  t.equal(characterReferenceInvalid[0x89], '‰');
-  t.equal(characterReferenceInvalid[0x99], '™');
+test('characterEntities', function(t) {
+  t.equal(characterReferenceInvalid[0x80], '€')
+  t.equal(characterReferenceInvalid[0x89], '‰')
+  t.equal(characterReferenceInvalid[0x99], '™')
 
-  t.end();
-});
+  t.end()
+})
