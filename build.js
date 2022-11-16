886c4aae610c076800c80671dc7dc689834425a6
Titus Wormer <tituswormer@gmail.com>
2022-11-16 10:17:28 +0100

    Refactor build script

--- build.js
+++ build.js
@@ -1,48 +1,42 @@
-import fs from 'node:fs'
-import https from 'node:https'
-import {bail} from 'bail'
-import concat from 'concat-stream'
-import {unified} from 'unified'
-import rehypeParse from 'rehype-parse'
+import fs from 'node:fs/promises'
+import fetch from 'node-fetch'
+import {fromHtml} from 'hast-util-from-html'
 import {selectAll} from 'hast-util-select'
 import {toString} from 'hast-util-to-string'
 
-/** @type {Record.<number, string>} */
+/** @type {Record<number, string>} */
 const data = {
   0: '�'
 }
 
-https.get('https://html.spec.whatwg.org/multipage/parsing.html', (response) => {
-  response.pipe(concat(onconcat)).on('error', bail)
-})
+const response = await fetch(
+  'https://html.spec.whatwg.org/multipage/parsing.html'
+)
+const text = await response.text()
 
-/**
- * @param {Buffer} buf
- */
-function onconcat(buf) {
-  const tree = unified().use(rehypeParse).parse(buf)
-  const rows = selectAll('#table-charref-overrides tbody tr', tree)
-  let index = -1
+const tree = fromHtml(text)
 
-  while (++index < rows.length) {
-    const cells = selectAll('td', rows[index])
+const rows = selectAll('#table-charref-overrides tbody tr', tree)
+let index = -1
 
-    data[Number.parseInt(toString(cells[0]).slice(2), 16)] =
-      String.fromCharCode(Number.parseInt(toString(cells[1]).slice(2), 16))
-  }
+while (++index < rows.length) {
+  const cells = selectAll('td', rows[index])
 
-  fs.writeFile(
-    'index.js',
-    [
-      '/**',
-      ' * Map of invalid numeric character references to their replacements, according to HTML.',
-      ' *',
-      ' * @type {Record<number, string>} ',
-      ' */',
-      'export const characterReferenceInvalid = ' +
-        JSON.stringify(data, null, 2),
-      ''
-    ].join('\n'),
-    bail
-  )
+  const key = Number.parseInt(toString(cells[0]).slice(2), 16)
+  const value = Number.parseInt(toString(cells[1]).slice(2), 16)
+
+  data[key] = String.fromCodePoint(value)
 }
+
+await fs.writeFile(
+  'index.js',
+  [
+    '/**',
+    ' * Map of invalid numeric character references to their replacements, according to HTML.',
+    ' *',
+    ' * @type {Record<number, string>} ',
+    ' */',
+    'export const characterReferenceInvalid = ' + JSON.stringify(data, null, 2),
+    ''
+  ].join('\n')
+)
